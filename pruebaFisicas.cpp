5d678beb27051e5caa3b0e58a0fe09ef3828d241
Muners24 <ramon_chunqui@outlook.com>
2023-11-24 01:35:01 -0800

    modified:   pruebaFisicas.cpp 	new file:   sonidos/musica-menu.mp3 	new file:   sonidos/musica.mp3 	new file:   sonidos/seleccion.wav 	new file:   sonidos/select.mp3 	new file:   sonidos/select.wav

--- pruebaFisicas.cpp
+++ pruebaFisicas.cpp
@@ -1,7 +1,5 @@
 
 #include "C:\raylib\raylib\src\raylib.h"
-#include <math.h>
-#include <stdlib.h>
 
 #define RANCHO 1280
 #define RALTO 720
@@ -9,84 +7,153 @@
 #define ANCHOP 50
 #define SUELO 650
 
+#define G 0.0981
 //blank transparente
 
 int main()
 {
     SetTargetFPS(60);  
     InitWindow(RANCHO,RALTO,"juego");
-    //posicion inicial
-    int xposP=600;
-    int yposP=SUELO-ALTURAP;
-    int frameCount=0;
-    int saltoFlag=0;
-    int saltoTime=0;
-
+    //Inicializa la posicion
+    int xposP=400;  //Posicion incial
+    int yposP=0;    //""
+    int y0=yposP;   //Posicion inicial para calcular la posicion
+    int time=0;     //contador
+    int bsuelo=0;   //bandera de tocar el suelo
+    int bplat1=0;   //bandera de tocar plataforma1
+    int bplat2=0;   //bandera de tocar plataforma2
+    int v0=0;       //velocidad inicial
     while(!WindowShouldClose())
     {
 
-        BeginDrawing;
+        BeginDrawing();
         ClearBackground(WHITE);
         DrawRectangle(0,SUELO,1280,70,GREEN);
-        DrawRectangle(xposP,yposP,ANCHOP,ALTURAP,BLUE);
-        int y0;
-        frameCount++;
+        DrawRectangle(400,400,350,40,GREEN);
+        DrawRectangle(200,200,350,40,GREEN);
+
+        time++;
+
+        if(!bsuelo) //si no esta en el suelo, calcula la poscion 
+        {
+            yposP=y0+v0*(time)+G*time*time; //y=y0+v0*t+G*t^2
+        }
+
+        //v=v0*2*G*t derivada de la posicion=velocidad
+        if(v0+2*G*time>0)   //Si esta cayendo == si la velocidad es menor a 0, en este caso mayor porque esta invertido y
+        {
+            if(yposP>=SUELO-ALTURAP)    //si la posicion esta por debajo del suelo
+            {
+                time=0;                 
+                yposP=SUELO-ALTURAP;    //ajusta la posicion exacta con el suelo
+                y0=yposP;               //la posicion inicial es el suelo
+                v0=0;                   //velocidad inicial = 0
+                bsuelo=1;               //esta tocando el suelo
+            }
+            else
+            {
+                if(yposP>=(400-ALTURAP))    //si esta tocando la altura de la plataforma n
+                {
+                    if(yposP<=(420-ALTURAP))  //Si esta dentro de la plataforma n
+                    {   
+                        if(xposP<=750)  //Si esta tocando el lado derecho de la plataforma n
+                        {
+                            if(400<=xposP+ANCHOP)   //si esta tocando el lado izquierdo de la platforma n
+                            {
+                                time=0;
+                                yposP=400-ALTURAP;  //ajusta la posicion sobre la plataforma n
+                                y0=yposP;           //posicion inicial sobfe la plataforma n
+                                v0=0;               //velocidad inicial = 0
+                                bplat1=1;           //esta tocando la plataforma 1
+                            }
+                        }
+                    }
+                }
+                
+                if(yposP>=(200-ALTURAP))    //lo mismo de arriba para otra plataforma
+                {
+                    if(yposP<=(220-ALTURAP))   
+                    {
+                        if(xposP<=550)
+                        {
+                            if(200<=xposP+ANCHOP)
+                            {
+                                time=0;
+                                yposP=200-ALTURAP;
+                                y0=yposP;
+                                v0=0;
+                                bplat2=1;
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        
+
+        if(IsKeyPressed(KEY_UP))
+        {
+            if(bsuelo)      //si esta tocando el suelo
+            {
+                v0=-11;     //inicializa los valores para el salto
+                y0=yposP;
+                time=0;     
+                bsuelo=0;   //cambia su bandera a 0
+            }
+            else
+            {
+                if(bplat1)  //bandera de plataforma n
+                {
+                    v0=-11;     //a
+                    y0=yposP;   //inicializa los valores para el salto
+                    time=0;
+                    bplat1=0;   //cambia la bandera de la plataforma n a 0
+                }
+                else
+                {
+                    if(bplat2)  
+                    {
+                        v0=-11;
+                        y0=yposP;
+                        time=0;
+                        bplat2=0;
+                    }
+                }
+            }
+        }
 
         if(IsKeyDown(KEY_RIGHT))
         {
-            //Si no topa el borde derecho
-            if((xposP+ANCHOP)!=1280)
+            
+            if((xposP+ANCHOP)<1280)    //Si no toca el borde derecho 
             {
                 xposP+=3;
+            }
+            else
+            {
+                xposP=1280-ANCHOP;  //Si se paso de la pantalla reajusta su posicion
             }
         }
         else
         {
             if(IsKeyDown(KEY_LEFT))
             {
-                //Si no topa el borde izquirdo
-                if(xposP!=0)
+                
+                if(xposP>=0)    //Si no toca el borde izquirdo
                 {
                     xposP-=3;
                 }
-            }
-        }
-
-        if(IsKeyPressed(KEY_UP))
-        {
-            saltoTime=0;
-            saltoFlag=1;    
-            y0=yposP;
-        }
-        
-        if(saltoFlag)
-        {  
-            saltoTime++;    
-
-            yposP=(pow((saltoTime),2))*0.7407/20-saltoTime*6.66666+y0;
-            if(saltoTime==180)
-            {
-                saltoFlag=0;
-                yposP=y0;
-                //free(&y0);
-            }
-            else
-            {
-                if((yposP-ALTURAP)==SUELO)
+                else
                 {
-                    saltoFlag=0;
-                    yposP=y0;
-                    //free(&y0);
+                    xposP=0;    //Si se paso de la pantalla reajusta su posicion
                 }
             }
         }
 
-        
-        if(frameCount=600)
-        {
-            frameCount=0;
-        }
-
+        DrawRectangle(xposP,yposP,ANCHOP,ALTURAP,BLUE); //Se muestra el personaje al final para realizar los ajustes primero
+    
         EndDrawing();
     }
     CloseWindow();
